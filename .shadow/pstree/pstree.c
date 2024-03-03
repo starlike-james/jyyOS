af163d19bdd2ae5e92574d1426d1013f5d07cf2b
tracer-nju <tracer@nju.edu.cn>
2024-03-03 16:01:19 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:19 up  4:02,  4 users,  load average: 0.30, 0.37, 0.36

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <getopt.h>
+#include <stdbool.h>
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
@@ -12,31 +13,22 @@ int main(int argc, char *argv[]) {
       {"show-pids"   , no_argument, NULL, 'p'},
       {"numeric-sort", no_argument, NULL, 'n'},
       {"version"     , no_argument, NULL, 'V'},
+      {0             , 0          , NULL,  0 },
   };
   int opt = 0;
+  bool show_pids_flag = false, nemuric_sorc_flag = false, version_flag = false;
   while((opt = getopt_long(argc, argv, "pnV", table, NULL)) != -1){
       switch (opt){
-          case 'p':
-              {
-                  printf("p\n");
-                  break;
-              }
-          case 'n':
-              {
-                  printf("n\n");
-                  break;
-              }
-          case 'V':
-              {
-                  printf("V\n");
-                  break;
-              }
-          default:
-              {
-                  printf("default: invalid argument\n");
-              }
-
+          case 'p': show_pids_flag = true; break;
+          case 'n': nemuric_sorc_flag = true; break;
+          case 'V': version_flag = true; break;
+          default: printf("default: invalid argument\n"); break;
       }
   }
+  if(version_flag == true){
+      fprintf(stderr, "wzy's pstree\n");
+      return 0;
+  }
+
   return 0;
 }
