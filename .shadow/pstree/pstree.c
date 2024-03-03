cd2d28927330df02da9440b683049160455167f9
tracer-nju <tracer@nju.edu.cn>
2024-03-03 17:10:38 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:38 up  5:12,  5 users,  load average: 0.75, 0.62, 0.55

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -3,7 +3,8 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include <dirent.h>
-
+#include <string.h>
+#include <ctype.h>
 
 bool show_pids_flag = false, nemuric_sort_flag = false, version_flag = false;
 
@@ -23,7 +24,18 @@ void parse_arg(int argc, char *argv[]){
           default: printf("default: invalid argument\n"); break;
       }
   }
+}
 
+bool isNumber(const char *str){
+    int len = strlen(str);
+    const char* digit = str;
+    for (int i = 0; i < len; i++){
+        if(!isdigit(*digit)){
+            return false;
+        }
+        digit++;
+    }
+    return true;
 }
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
@@ -40,7 +52,19 @@ int main(int argc, char *argv[]) {
   }
 
   DIR *directory;
-  
+  struct dirent *entry;
+
+  directory = opendir("/proc");
+  if(directory == NULL){
+      perror("opendir");
+      return 1;
+  }
+  while((entry = readdir(directory)) != NULL){
+      char *d_name = entry->d_name;
+      if(isNumber(d_name)){
+          printf("proc/%s", d_name);
+      }
+  }
 
   
 
