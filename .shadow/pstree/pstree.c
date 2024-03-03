b2e436b492b3179e49dc53136186cf624ef8821f
tracer-nju <tracer@nju.edu.cn>
2024-03-03 16:33:42 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:42 up  4:35,  5 users,  load average: 0.39, 0.58, 0.54

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -2,21 +2,19 @@
 #include <assert.h>
 #include <getopt.h>
 #include <stdbool.h>
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
+#include <dirent.h>
 
-  const struct option table[] = {
+
+bool show_pids_flag = false, nemuric_sort_flag = false, version_flag = false;
+
+void parse_arg(int argc, char *argv[]){
+   const struct option table[] = {
       {"show-pids"   , no_argument, NULL, 'p'},
       {"numeric-sort", no_argument, NULL, 'n'},
       {"version"     , no_argument, NULL, 'V'},
       {0             , 0          , NULL,  0 },
   };
   int opt = 0;
-  bool show_pids_flag = false, nemuric_sort_flag = false, version_flag = false;
   while((opt = getopt_long(argc, argv, "pnV", table, NULL)) != -1){
       switch (opt){
           case 'p': show_pids_flag = true; break;
@@ -25,10 +23,26 @@ int main(int argc, char *argv[]) {
           default: printf("default: invalid argument\n"); break;
       }
   }
+
+}
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    assert(argv[i]);
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  assert(!argv[argc]);
+
+  parse_arg(argc, argv);
+ 
   if(version_flag == true){
       fprintf(stderr, "wzy's pstree\n");
       return 0;
   }
+
+  DIR *directory;
+  
+
+  
 
   return 0;
 }
