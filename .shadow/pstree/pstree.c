1399f6ae8bfaed0b52d81061ef5aaf457c04e370
tracer-nju <tracer@nju.edu.cn>
2024-03-03 15:46:50 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:50 up  3:48,  4 users,  load average: 0.13, 0.34, 0.35

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <assert.h>
-
+#include <getopt.h>
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+
+  const struct option table[] = {
+      {"show-pids"   , no_argument, NULL, 'p'},
+      {"numeric-sort", no_argument, NULL, 'n'},
+      {"version"     , no_argument, NULL, 'V'},
+  };
+  int opt = 0;
+  while((opt = getopt_long(argc, argv, "pnV", table, NULL))){
+      switch (opt){
+          case 'p':
+              {
+                  printf("p\n");
+              }
+          case 'n':
+              {
+                  printf("n\n");
+              }
+          case 'V':
+              {
+                  printf("V\n");
+              }
+
+      }
+  }
   return 0;
 }
