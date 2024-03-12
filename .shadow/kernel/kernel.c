89f4520ea941311316f85f60848a8a2e8302cda9
tracer-nju <tracer@nju.edu.cn>
2024-03-12 22:27:35 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:35 up 12:18,  4 users,  load average: 0.20, 0.44, 0.57

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -19,6 +19,9 @@ void print_key() {
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown) {
+      if(event.keycode == AM_KEY_ESCAPE){
+          halt(0);
+      }
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
