9442cc7c634a8d9cceaf1197d6492e10ac8972a9
tracer-nju <tracer@nju.edu.cn>
2024-03-12 23:59:56 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:59:56 up 13:50,  5 users,  load average: 0.68, 0.78, 0.71

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -67,7 +67,7 @@ void splash() {
         int x = w * pic_x / pic_width;
         int y = h * pic_y / pic_height;
         uint32_t pixels[1];
-        uint8_t* pic_src = pic_bin + pic_x + pic_y * pic_width;
+        uint8_t* pic_src = pic_bin + (pic_x + pic_y * pic_width) * sizeof(uint32_t);
         memcpy(pixels, pic_src, 4);
         pixels[0] = pic_bin[pic_x + pic_y * pic_width];
         AM_GPU_FBDRAW_T event = {
