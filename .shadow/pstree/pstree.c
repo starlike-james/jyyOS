61a572e8e3c91b681c3a64c04ce570f9d05f69f9
tracer-nju <tracer@nju.edu.cn>
2024-03-24 00:52:43 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:52:43 up 3 days,  6:29,  4 users,  load average: 0.42, 0.47, 0.75

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -200,7 +200,6 @@ void traversal(TreeNode *current){
     dep--;
 }
 int main(int argc, char *argv[]) {
-    assert(0);
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
         printf("argv[%d] = %s\n", i, argv[i]);
