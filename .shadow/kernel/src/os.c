36ce451b809b424e2af772116d746b2888a1d4ea
tracer-nju <tracer@nju.edu.cn>
2024-04-19 15:37:24 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:24 up 14:27,  4 users,  load average: 0.61, 0.68, 0.62

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -5,9 +5,11 @@ static void os_init() {
 }
 
 static void os_run() {
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-        putch(*s == '*' ? '0' + cpu_current() : *s);
-    }
+
+    printf("Hello World from CPU #%d\n", cpu_current());
+    //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    //    putch(*s == '*' ? '0' + cpu_current() : *s);
+    //}
     while (1) ;
 }
 
