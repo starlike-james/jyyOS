094a2eb62e65de1208482bb36edd8f731aa52211
tracer-nju <tracer@nju.edu.cn>
2024-04-22 20:59:09 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:09 up 4 days, 11:49,  4 users,  load average: 0.53, 0.73, 1.18

--- .shadow/kernel/src/centrallist.c
+++ .shadow/kernel/src/centrallist.c
@@ -79,7 +79,7 @@ void *central_allocate(size_t size, bool slab){
             }
 
             uintptr_t ptr = ROUNDUP((uintptr_t)cur + SLAB_PAGE, align);
-            printf("ptr = %x cur = %x align = %x\n", (uintptr_t)ptr, (uintptr_t)cur, align);
+            printf("alloc: ptr = %x cur = %x align = %x\n", (uintptr_t)ptr, (uintptr_t)cur, align);
             //uintptr_t alloc_size = ROUNDUP(size, SLAB_PAGE); 
             if((uintptr_t)cur + cur->size < ptr + alloc_size){
                 continue;
@@ -168,6 +168,7 @@ void central_free(void *ptr, bool slab){
         prev = cur;
         cur = cur->next;
     }
+    printf("free: ptr = %x cur = %x prev = %x\n", (uintptr_t)ptr, (uintptr_t)cur, (uintptr_t)prev);
 
     if(prev == NULL){
         central.head = header;
