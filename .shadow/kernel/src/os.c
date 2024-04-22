5bb73f6f03e710fbc2edd68e36be39e0b3c28092
tracer-nju <tracer@nju.edu.cn>
2024-04-22 23:09:33 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:09:33 up 28 min,  3 users,  load average: 0.60, 0.90, 0.78

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -9,43 +9,43 @@ static void os_run() {
 
     printf("Hello World from CPU #%d\n", cpu_current());
     size_t align = 1 << 8;
-        void *ptr[1024];
-        memset(ptr, 0, 1024);
-        int i = 0;
-        for(i = 0; i < 4; i++){
-            ptr[i] = pmm->alloc(align);
-            printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
-            align = align / 2;
-            i++;
-            ptr[i] = pmm->alloc(align);
-            printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
-            align = align * 2;
-            if(ptr[i] == NULL){
-                break;
-            }
+    void *ptr[1024];
+    memset(ptr, 0, 1024);
+    int i = 0;
+    for(i = 0; i < 4; i++){
+        ptr[i] = pmm->alloc(align);
+        printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+        align = align / 2;
+        i++;
+        ptr[i] = pmm->alloc(align);
+        printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+        align = align * 2;
+        if(ptr[i] == NULL){
+            break;
         }
-        size_t tem = align;
-        for(; i < 8; i++){
-            ptr[i] = pmm->alloc(align);
-            printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
-            align++;
-            if(ptr[i] == NULL){
-                break;
-            }
+    }
+    size_t tem = align;
+    for(; i < 8; i++){
+        ptr[i] = pmm->alloc(align);
+        printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+        align++;
+        if(ptr[i] == NULL){
+            break;
         }
-        for(int i = 0; i < 8; i++){
-            if(ptr[i] == NULL){
-                continue;
-            }
-            pmm->free(ptr[i]);
-            printf("pmm free %x success!, ptr = %x\n", align, ptr[i]);
+    }
+    for(int i = 0; i < 8; i++){
+        if(ptr[i] == NULL){
+            continue;
         }
-        align = tem;
-        align = align << 1;
+        pmm->free(ptr[i]);
+        printf("pmm free %x success!, ptr = %x\n", align, ptr[i]);
+    }
+    align = tem;
+    align = align << 1;
 
     printf("finish!\n");
     while(align < (1024 * KiB)){
-            }
+    }
    //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     //    putch(*s == '*' ? '0' + cpu_current() : *s);
     //}
