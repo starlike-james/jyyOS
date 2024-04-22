7576ca9b3b327856d87410f15f053248b0cf137e
tracer-nju <tracer@nju.edu.cn>
2024-04-22 22:14:06 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:14:06 up 4 days, 13:04,  4 users,  load average: 2.46, 2.11, 1.84

--- .shadow/kernel/src/centrallist.c
+++ .shadow/kernel/src/centrallist.c
@@ -144,6 +144,7 @@ void central_free(void *ptr, bool slab){
         // big memory's header is a SLAB_PAGE before the ptr_addr
         header = (block_t*)((uintptr_t)ptr - SLAB_PAGE);
         assert((header->size & SLAB_MASK) == 0 && header->size >= SLAB_PAGE);
+
         assert(header->magic == BIG_MEM);
         assert(header->next == NULL);
         
