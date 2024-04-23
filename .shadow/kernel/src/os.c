6087609cacd31a6dce589f3b3fa7893a1a82e7b2
tracer-nju <tracer@nju.edu.cn>
2024-04-23 20:17:17 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:17 up 21:36,  4 users,  load average: 0.44, 0.56, 0.97

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -32,12 +32,16 @@ static void os_run() {
         int i = 0;
         for(i = 0; i < 512; i++){
             ptr[i] = pmm->alloc(align);
-            memset(ptr[i], 100, align);
+            if(ptr[i] != NULL){
+                memset(ptr[i], 100, align);     
+            }
             //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align = align / 2;
             i++;
             ptr[i] = pmm->alloc(align);
-            memset(ptr[i], 100, align);
+            if(ptr[i] != NULL){
+                memset(ptr[i], 100, align);
+            }
             //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align = align * 2;
             if(ptr[i] == NULL){
@@ -47,7 +51,9 @@ static void os_run() {
         size_t tem = align;
         for(; i < 1024; i++){
             ptr[i] = pmm->alloc(align);
-            memset(ptr[i], 100, align);
+            if(ptr[i] != NULL){
+                memset(ptr[i], 100, align);
+            }
             //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align++;
             if(ptr[i] == NULL){
