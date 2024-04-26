32fd166d0bc55e4832c6ad6ae79b37a17df0cef5
tracer-nju <tracer@nju.edu.cn>
2024-04-26 18:46:08 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:46:08 up 1 day, 15:36,  5 users,  load average: 0.34, 0.33, 0.19

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,9 +1,7 @@
 #include <common.h>
 #include <macro.h>
 
-static void os_init() {
-    pmm->init();
-}
+static void os_init() { pmm->init(); }
 void *ptr_all[8][1024];
 
 static void os_run() {
@@ -26,58 +24,58 @@ static void os_run() {
           printf("pmm free %x success!, ptr = %x\n", align, ptr[i]);
       }*/
 
-    
-    while(align < (4096 * KiB)){
+    while (align < (4096 * KiB)) {
         memset(ptr, 0, 1024 * sizeof(void *));
         int i = 0;
-        for(i = 0; i < 512; i++){
+        for (i = 0; i < 512; i++) {
             ptr[i] = pmm->alloc(align);
-            if(ptr[i] != NULL){
-                memset(ptr[i], 100, align);     
+            if (ptr[i] != NULL) {
+                memset(ptr[i], 100, align);
             }
-            //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+            // printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align = align / 2;
             i++;
             ptr[i] = pmm->alloc(align);
-            if(ptr[i] != NULL){
+            if (ptr[i] != NULL) {
                 memset(ptr[i], 100, align);
             }
-            //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+            // printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align = align * 2;
-            if(ptr[i] == NULL){
+            if (ptr[i] == NULL) {
                 break;
             }
         }
         size_t tem = align;
-        for(; i < 1024; i++){
+        for (; i < 1024; i++) {
             ptr[i] = pmm->alloc(align);
-            if(ptr[i] != NULL){
+            if (ptr[i] != NULL) {
                 memset(ptr[i], 100, align);
             }
-            //printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
+            // printf("pmm alloc %x success!, ptr = %x\n", align, ptr[i]);
             align++;
-            if(ptr[i] == NULL){
+            if (ptr[i] == NULL) {
                 break;
             }
         }
-        for(int i = 0; i < 1024; i++){
-            if(ptr[i] == NULL){
+        for (int i = 0; i < 1024; i++) {
+            if (ptr[i] == NULL) {
                 continue;
             }
             pmm->free(ptr[i]);
-            //printf("pmm free %x success!, ptr = %x\n", align, ptr[i]);
+            // printf("pmm free %x success!, ptr = %x\n", align, ptr[i]);
         }
         align = tem;
         align = align << 1;
     }
-   //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-    //    putch(*s == '*' ? '0' + cpu_current() : *s);
-    //}
+    // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    //     putch(*s == '*' ? '0' + cpu_current() : *s);
+    // }
     printf("finish!\n");
-    while (1) ;
+    while (1)
+        ;
 }
 
 MODULE_DEF(os) = {
     .init = os_init,
-    .run  = os_run,
+    .run = os_run,
 };
