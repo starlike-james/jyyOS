28af0c363701733fdae092e80f585df1df884d7f
tracer-nju <tracer@nju.edu.cn>
2024-05-07 00:58:52 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:58:52 up 3 days,  7:22,  4 users,  load average: 0.07, 0.28, 0.37

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char template[] = "/tmp/creplXXXXXX";
 
 int main(int argc, char *argv[]) {
     static char line[4096];
@@ -8,11 +13,38 @@ int main(int argc, char *argv[]) {
         printf("crepl> ");
         fflush(stdout);
 
+        bool func = false;
+
         if (!fgets(line, sizeof(line), stdin)) {
             break;
         }
 
+        if(strncmp(line, "int", 3) == 0){
+            func = true;
+        }
+
+        int fd = mkstemp(template);
+
+        if (fd == -1) {
+            perror("mkstemp");
+            exit(EXIT_FAILURE);
+        }
+        printf("临时文件名：%s\n", template);
+        
+
+        
+
+        
+
+
         // To be implemented.
         printf("Got %zu chars.\n", strlen(line));
+        
+        close(fd);
+
+        if(remove(template) == -1){
+            perror("remove");
+            exit(EXIT_FAILURE);
+        }
     }
 }
