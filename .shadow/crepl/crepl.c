8f196d3e89094fdf40c108e7747d97610c0992fa
tracer-nju <tracer@nju.edu.cn>
2024-05-07 20:17:53 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:17:53 up 4 days,  2:41,  4 users,  load average: 0.74, 0.55, 0.47

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -111,10 +111,17 @@ int main(int argc, char *argv[]) {
         }
         
         void *handle = dyload(dyfilename);
+        
         if(!is_func){
             int (*expr_func)(void);
             *(void **)(&expr_func) = dlsym(handle, expr_func_name);
             int res = expr_func();
+
+            char* error;
+            if((error = dlerror()) != NULL){
+                fprintf(stderr, "%s\n", error);
+            }
+
             printf("= %d\n", res);
         }
         else{
