4c27ce00ae1a3f98c4035ba3b27ede5f42faa6c0
tracer-nju <tracer@nju.edu.cn>
2024-05-16 23:11:12 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:12 up  6:08,  4 users,  load average: 0.52, 0.38, 0.46

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -191,6 +191,25 @@ int main(int argc, char *argv[]) {
                 fflush(stdout);
             }
         }
+        
+        int total_time = 0;
+        for(int i = 0; i < nrcall; i++){
+            total_time += call[i].time;
+        }
+
+        nrtime++;
+        qsort(call, nrcall, sizeof(syscall_t), cmp);
+        printf("Time: %d.%ds\n", nrtime / 10, nrtime % 10);
+        for(int i = 0; i < 5; i++){
+            printf("%s (%d%%)\n", call[i].name, call[i].time * 100 / total_time);
+        }
+        printf("...\n");
+        printf("====================\n");
+        for(int i = 0; i < 80; i++){
+            putchar('\0');
+        }
+        last = current;
+        fflush(stdout);
         fclose(stream);
     }
 
