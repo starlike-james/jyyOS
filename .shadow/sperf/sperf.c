3716a1006a3d9cea64aaf5b323f1466b968b1ba7
tracer-nju <tracer@nju.edu.cn>
2024-05-16 19:40:24 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:24 up  2:38,  4 users,  load average: 0.50, 0.59, 0.46

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,17 +1,39 @@
-#include <stdio.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
-#include <string.h>
 
 extern char **environ;
 // char cmd[100] = "";
 // char *path;
 int main(int argc, char *argv[]) {
     char **env = environ;
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
     pid_t pid = fork();
-    if(pid == 0){
+
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+
+        close(stderr);
+        dup(pipefd[1]);
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        close(stdout);
+        int fd = open("/dev/null", O_WRONLY);
+        dup(fd);
 
         argv[0] = "strace";
 
@@ -21,16 +43,32 @@ int main(int argc, char *argv[]) {
 
         char cmd[100] = "";
         char *prefix = strtok(path, ":");
-        while (prefix != NULL){
+        while (prefix != NULL) {
             sprintf(cmd, "%s/strace", prefix);
             execve(cmd, argv, env);
             prefix = strtok(NULL, ":");
         }
+        printf("strace not found in PATH.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        // close(stdin);
+        // dup(pipefd[0]);
+        // close(pipefd[0]);
+        close(pipefd[1]);
+
+        char buf[1024];
+        FILE *stream = fdopen(pipefd[0], "r");
+        if (!stream) {
+            perror("fdopen");
+            exit(EXIT_FAILURE);
+        }
+        // fgets(buf, sizeof(buf), stream);
+        while (fgets(buf, sizeof(buf), stream) != NULL) {
+            printf("%s", buf);
+        }
+        fclose(stream);
+
     }
 
-
-    
     return 0;
 }
-
-
