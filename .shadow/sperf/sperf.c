b57dfb3ee243573d9f2a838df8fa2152e1d6b397
tracer-nju <tracer@nju.edu.cn>
2024-05-16 19:04:41 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:41 up  2:02,  3 users,  load average: 0.20, 0.33, 0.41

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 #include <string.h>
 
 extern char **environ;
-char cmd[100] = "";
-char *path;
+// char cmd[100] = "";
+// char *path;
 int main(int argc, char *argv[]) {
     char **env = environ;
-    for (int i = 0; i < argc; i++) {
-        assert(argv[i]);
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-    assert(!argv[argc]);
-    argv[0] = "strace";
+    pid_t pid = fork();
+    if(pid == 0){
 
+        argv[0] = "strace";
 
-    const char *PATH = getenv("PATH");
-    path = malloc(strlen(PATH));
-    strcpy(path, PATH);
-    char *prefix = strtok(path, ":");
-    while (prefix != NULL){
-        sprintf(cmd, "%s/strace", prefix);
-        execve(cmd, argv, env);
-        prefix = strtok(NULL, ":");
+        const char *PATH = getenv("PATH");
+        char *path = malloc(strlen(PATH));
+        strcpy(path, PATH);
+
+        char cmd[100] = "";
+        char *prefix = strtok(path, ":");
+        while (prefix != NULL){
+            sprintf(cmd, "%s/strace", prefix);
+            execve(cmd, argv, env);
+            prefix = strtok(NULL, ":");
+        }
     }
 
 
