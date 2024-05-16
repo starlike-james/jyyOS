9513dd7a0f5263f1067b21384c616b7108b46bef
tracer-nju <tracer@nju.edu.cn>
2024-05-16 18:34:41 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:41 up  1:32,  3 users,  load average: 0.25, 0.40, 0.65

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
+char *CMD;
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         assert(argv[i]);
         printf("argv[%d] = %s\n", i, argv[i]);
     }
     assert(!argv[argc]);
+
     return 0;
 }
+
+
