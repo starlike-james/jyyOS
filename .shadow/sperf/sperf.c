3661c90c7cbbf0a065177a5193881262870251d1
tracer-nju <tracer@nju.edu.cn>
2024-05-16 18:45:08 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:08 up  1:42,  3 users,  load average: 0.36, 0.43, 0.56

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-char *CMD;
+char *cmd;
+char **args;
+
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-        assert(argv[i]);
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
     assert(!argv[argc]);
 
+    cmd = argv[1];
+    args = argv + 1;
+
+    printf("%s\n", cmd);
+    for (int i = 0; i < argc - 1; i++) {
+        assert(args[i]);
+        printf("args[%d] = %s\n", i, args[i]);
+    }
+    assert(!args[argc - 1]);
     const char *path = getenv("PATH");
     printf("%s\n", path);
     return 0;
 }
-
-
