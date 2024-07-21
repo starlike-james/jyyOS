1b44397d0e7586be0f3846468efc3502269530c7
tracer-nju <tracer@nju.edu.cn>
2024-07-21 21:29:52 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:52 up 6 days, 10:45,  7 users,  load average: 0.26, 0.56, 0.58

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,8 +1,8 @@
 #include "am.h"
 #include "arch/x86_64-qemu.h"
 #include <common.h>
-#include <os.h>
 #include <macro.h>
+#include <os.h>
 #include <signal.h>
 #include <stdint.h>
 
@@ -11,20 +11,30 @@
 // extern task_t idle[];
 // extern tasklist_t tasklist[];
 
-static inline task_t *task_alloc(){
-    return pmm->alloc(sizeof(task_t));
-}
+static inline task_t *task_alloc() { return pmm->alloc(sizeof(task_t)); }
 
 static sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
 
-static void T_produce(void *arg) { while (1) { P(&empty); putch('('); V(&fill);  } }
-static void T_consume(void *arg) { while (1) { P(&fill);  putch(')'); V(&empty); } }
+static void T_produce(void *arg) {
+    while (1) {
+        P(&empty);
+        // putch('(');
+        V(&fill);
+    }
+}
+static void T_consume(void *arg) {
+    while (1) {
+        P(&fill);
+        // putch(')');
+        V(&empty);
+    }
+}
 
 static void run_test1() {
     kmt->sem_init(&empty, "empty", 3);
-    kmt->sem_init(&fill,  "fill",  0);
+    kmt->sem_init(&fill, "fill", 0);
     for (int i = 0; i < 5; i++) {
         kmt->create(task_alloc(), "producer", T_produce, NULL);
     }
@@ -40,34 +50,33 @@ static void run_test1() {
 //
 // static void T1(void *arg) {
 //     int i = 0;
-//     while (1) { 
-//         putch('A'); 
+//     while (1) {
+//         putch('A');
 //         iset(true);
 //         if(i == 0){
 //             iset(false);
-//             yield(); 
+//             yield();
 //             i = 1;
 //         }
 //         delay();
-//     } 
+//     }
 // }
 //
 // static void T2(void *arg) { while (1) { putch('B'); delay(); } }
 // static void T3(void *arg) { while (1) { putch('C'); delay(); } }
 //
-static void os_init() { 
+static void os_init() {
     pmm->init();
     kmt->init();
-    
+
 #ifdef DEBUG
     // for(int i = 0; i < 3; i++){
-    //     kmt->create(task_alloc(), "A", T1, NULL); 
+    //     kmt->create(task_alloc(), "A", T1, NULL);
     //     kmt->create(task_alloc(), "B", T2, NULL);
     //     kmt->create(task_alloc(), "C", T3, NULL);
     // }
     run_test1();
 #endif
-
 }
 
 void *ptr_all[8][1024];
@@ -75,13 +84,13 @@ void *ptr_all[8][1024];
 static void os_run() {
     // logging("Hello World from CPU #%d\n", cpu_current());
     iset(true);
-    while (1);
+    while (1)
+        ;
 }
-
 
 handlerlist_t *handlerlist = NULL;
 
-static bool sane_context(Context *ctx){
+static bool sane_context(Context *ctx) {
     return ctx->rip >= 0x100000 && ctx->rip < (uintptr_t)heap.start;
 }
 
@@ -93,40 +102,40 @@ static Context *os_trap(Event ev, Context *ctx) {
         if (cur->event == EVENT_NULL || cur->event == ev.event) {
             Context *r = cur->handler(ev, ctx);
             panic_on(r && next, "returning multiple contexts");
-            if (r) next = r;
+            if (r)
+                next = r;
         }
-        if(cur->next != NULL){
+        if (cur->next != NULL) {
             assert(cur->seq <= cur->next->seq);
         }
         cur = cur->next;
     }
-
 
     panic_on(!next, "returning NULL context");
     dpanic_on(!sane_context(next), "returning to invalid context");
     return next;
 }
 
-static void os_on_irq(int seq, int event, handler_t handler){
+static void os_on_irq(int seq, int event, handler_t handler) {
     handlerlist_t *cur = pmm->alloc(sizeof(handlerlist_t));
     cur->seq = seq;
     cur->event = event;
     cur->handler = handler;
     cur->next = NULL;
 
-    if(handlerlist == NULL || cur->seq <= handlerlist->seq){
+    if (handlerlist == NULL || cur->seq <= handlerlist->seq) {
         cur->next = handlerlist;
         handlerlist = cur;
         return;
     }
 
     handlerlist_t *h = handlerlist;
-    while(h->next != NULL && h->next->seq < cur->seq){
+    while (h->next != NULL && h->next->seq < cur->seq) {
         h = h->next;
     }
 
     cur->next = h->next;
-    h->next = cur; 
+    h->next = cur;
     return;
 }
 
