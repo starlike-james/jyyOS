b2c1bdadede67d64eee4211f2fb0d67cc54f3308
tracer-nju <tracer@nju.edu.cn>
2024-07-21 21:31:07 +0800

    Linux jameswu-NbDE-WXX9 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:07 up 6 days, 10:46,  7 users,  load average: 0.37, 0.53, 0.57

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -33,12 +33,12 @@ static void T_consume(void *arg) {
 }
 
 static void run_test1() {
-    kmt->sem_init(&empty, "empty", 3);
+    kmt->sem_init(&empty, "empty", 1);
     kmt->sem_init(&fill, "fill", 0);
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 1; i++) {
         kmt->create(task_alloc(), "producer", T_produce, NULL);
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 1; i++) {
         kmt->create(task_alloc(), "consumer", T_consume, NULL);
     }
 }
